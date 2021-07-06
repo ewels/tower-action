8c051350b16b00829d6f511b99992e46493fa4e4
Phil Ewels <phil.ewels@scilifelab.se>
2021-07-06 12:20:19 +0200

    No newlines at all

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-curl \
-    --silent \
-    --show-error \
-    --fail \
-    -X POST "https://api.tower.nf/workflow/launch?workspaceId=${TOWER_WORKSPACE_ID}" \
-    -H "Accept: application/json" \
-    -H "Authorization: Bearer ${TOWER_BEARER_TOKEN}" \
-    -H "Content-Type: application/json" \
-    -d '{"launch": {"computeEnvId": "'${TOWER_COMPUTE_ENV}'","pipeline": "'${PIPELINE}'","workDir": "'${WORKDIR}'","paramsText": "'${PARAMETERS}'","revision": "'${REVISION}'","configProfiles": '${PROFILES}',"resume": false}}'
+curl --silent --show-error --fail -X POST "https://api.tower.nf/workflow/launch?workspaceId=${TOWER_WORKSPACE_ID}" -H "Accept: application/json" -H "Authorization: Bearer ${TOWER_BEARER_TOKEN}" -H "Content-Type: application/json" -d '{"launch": {"computeEnvId": "'${TOWER_COMPUTE_ENV}'","pipeline": "'${PIPELINE}'","workDir": "'${WORKDIR}'","paramsText": "'${PARAMETERS}'","revision": "'${REVISION}'","configProfiles": '${PROFILES}',"resume": false}}'
