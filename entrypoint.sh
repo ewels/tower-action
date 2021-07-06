9733b0f3e7eeac1bccb3de98bb58bc820f9f93cd
Phil Ewels <phil.ewels@scilifelab.se>
2021-07-06 12:22:00 +0200

    Build the JSON payload into a bash env first

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-curl --silent --show-error --fail -X POST "https://api.tower.nf/workflow/launch?workspaceId=${TOWER_WORKSPACE_ID}" -H "Accept: application/json" -H "Authorization: Bearer ${TOWER_BEARER_TOKEN}" -H "Content-Type: application/json" -d '{"launch": {"computeEnvId": "'${TOWER_COMPUTE_ENV}'","pipeline": "'${PIPELINE}'","workDir": "'${WORKDIR}'","paramsText": "'${PARAMETERS}'","revision": "'${REVISION}'","configProfiles": '${PROFILES}',"resume": false}}'
+$PAYLOAD='{"launch": {"computeEnvId": "'${TOWER_COMPUTE_ENV}'","pipeline": "'${PIPELINE}'","workDir": "'${WORKDIR}'","paramsText": "'${PARAMETERS}'","revision": "'${REVISION}'","configProfiles": '${PROFILES}',"resume": false}}'
+
+echo $PAYLOAD
+
+curl
+    --silent
+    --show-error
+    --fail
+    -X POST "https://api.tower.nf/workflow/launch?workspaceId=${TOWER_WORKSPACE_ID}"
+    -H "Accept: application/json"
+    -H "Authorization: Bearer ${TOWER_BEARER_TOKEN}"
+    -H "Content-Type: application/json"
+    -d $PAYLOAD
