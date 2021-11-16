070de2bdb21398850abb05947ff4adf8f97d5531
Phil Ewels <phil.ewels@scilifelab.se>
2021-11-16 11:26:13 +0100

    Use `--fail-with-body`

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ PAYLOAD=$(
 curl \
     --silent \
     --show-error \
-    --fail \
+    --fail-with-body \
     -X POST 'https://api.tower.nf/workflow/launch?'${TOWER_WORKSPACE_ID:+workspaceId="$TOWER_WORKSPACE_ID"} \
     -H "Accept: application/json" \
     -H "Authorization: Bearer ${TOWER_BEARER_TOKEN}" \
